dcda6fe88d9f9145476d28f7d6fef7177429f95d
Shivam <sk828763@gmail.com>
2023-05-04 14:45:52 -0600

    second commikt

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dbConnection = ()=>{
     try{
         const db =  async()=>{
-            await mongoose.connect('"mongodb+srv://Second:12345@mern.kehorho.mongodb.net/test"')
+            await mongoose.connect("mongodb+srv://Second:12345@mern.kehorho.mongodb.net/test")
             console.log("database is connected...");
         };
         return db;

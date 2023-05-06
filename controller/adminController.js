e8a28c56c0aa90612e9c8e469460b554cd7d692f
Shivam <sk828763@gmail.com>
2023-05-06 14:30:47 -0600

    Fourt Commit

--- controller/adminController.js
+++ controller/adminController.js
@@ -60,7 +60,7 @@ const viewUserProfile = async(req,res)=>{
     try {
         const user = await model.findById(req.params.id);
         if(user){
-            res.render('./user/viewProfile',{data:user})
+            res.render('./user/UserViewProfile',{data:user})
 
         }else{
             res.send({message:"User Not Found"})
